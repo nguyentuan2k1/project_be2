20c1add5fc93650b02c5f80d71726c2c2ccda5e8
ducnguyen4301 <duc040301@gmail.com>
2021-07-02 00:40:27 +0700

    done delete

---
+++ project_be2_nhomC/resources/views/subject.blade.php
@@ -0,0 +1,28 @@
+@extends('dashboard')
+    @section('content')
+    <div class="table-responsive ">
+        <table  style="margin: 0 100px" class="table">
+            <thead>
+                <tr>
+                    <th>subject_id</th>
+                    <th>subject_name</th>
+                    <th>status</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php //dd($subject); ?>
+                @foreach($subject as $subjects)
+                <tr>
+                    <td>{!! $subjects->subject_id !!}</td>
+                    <td>{!! $subjects->subject_name !!}</td>
+                    <td>{!! $subjects->status !!}</td>
+                    <td>
+                        <!-- Delete -->
+                        <a href="{{ route('subjects.delete',$subjects->subject_id) }}" class="btn btn-sm btn-danger">Delete</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    @endsection
