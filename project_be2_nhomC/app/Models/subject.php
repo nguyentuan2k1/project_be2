84f18c7fe7fb636a0ac94164636b197bc6563401
ducnguyen4301 <duc040301@gmail.com>
2021-07-01 23:32:42 +0700

    test

--- project_be2_nhomC/app/Models/subject.php
+++ project_be2_nhomC/app/Models/subject.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class subject extends Model
 {
     use SoftDeletes;
     use HasFactory;
     protected $dates = ['deleted_at'];
+
+    protected $table = 'subject';
 }
